663fd6030e5f8a2a6ce835f10f727429a5563b63
Anton <kb.nikitinav@mail.ru>
2019-03-11 23:28:55 +0300

    переработана функция обработки сенсорных кнопок + разбита на подфункции

--- GPIO.h
+++ GPIO.h
@@ -18,7 +18,7 @@ typedef enum enumLevel	{
 typedef struct struct_GPIO	{
 	byte *DDR;
 	byte *PORT;
-	byte *PIN;
+	byte *STATUS;
 	byte pinNumber;
 	} struct_GPIO;
 	
@@ -26,127 +26,147 @@ typedef struct struct_GPIO	{
 	struct_GPIO B0 = {
 		.DDR = &DDRB,
 		.PORT = &PORTB,
+		.STATUS = &PINB,
 		.pinNumber = 0b00000001
 	};
 	
 	struct_GPIO B1 = {
 		.DDR = &DDRB,
 		.PORT = &PORTB,
+		.STATUS = &PINB,
 		.pinNumber = 0b00000010
 	};
 	
 	struct_GPIO B2 = {
 		.DDR = &DDRB,
 		.PORT = &PORTB,
+		.STATUS = &PINB,
 		.pinNumber = 0b00000100
 	};
 	
 	struct_GPIO B3 = {
 		.DDR = &DDRB,
 		.PORT = &PORTB,
+		.STATUS = &PINB,
 		.pinNumber = 0b00001000
 	};
 	
 	struct_GPIO B4 = {
 		.DDR = &DDRB,
 		.PORT = &PORTB,
+		.STATUS = &PINB,
 		.pinNumber = 0b00010000
 	};
 	
 	struct_GPIO B6 = {
 		.DDR = &DDRB,
 		.PORT = &PORTB,
+		.STATUS = &PINB,
 		.pinNumber = 0b01000000
 	};
 	
 	struct_GPIO B7 = {
 		.DDR = &DDRB,
 		.PORT = &PORTB,
+		.STATUS = &PINB,
 		.pinNumber = 0b10000000
 	};
 	
 	struct_GPIO C0 = {
 		.DDR = &DDRC,
 		.PORT = &PORTC,
+		.STATUS = &PINC,
 		.pinNumber = 0b00000001
 		};
 		
 	struct_GPIO C1 = {
 		.DDR = &DDRC,
 		.PORT = &PORTC,
+		.STATUS = &PINC,
 		.pinNumber = 0b00000010
 	};
 	
 	struct_GPIO C2 = {
 		.DDR = &DDRC,
 		.PORT = &PORTC,
+		.STATUS = &PINC,
 		.pinNumber = 0b00000100
 	};
 	
 	struct_GPIO C3 = {
 		.DDR = &DDRC,
 		.PORT = &PORTC,
+		.STATUS = &PINC,
 		.pinNumber = 0b00001000
 	};
 	
 	struct_GPIO C4 = {
 		.DDR = &DDRC,
 		.PORT = &PORTC,
+		.STATUS = &PINC,
 		.pinNumber = 0b00010000
 	};
 	
 	struct_GPIO C5 = {
 		.DDR = &DDRC,
 		.PORT = &PORTC,
+		.STATUS = &PINC,
 		.pinNumber = 0b00100000
 	};
 	
 	struct_GPIO D0 = {
 		.DDR = &DDRD,
 		.PORT = &PORTD,
+		.STATUS = &PIND,
 		.pinNumber = 0b00000010
 	};
 	
 	struct_GPIO D1 = {
 		.DDR = &DDRD,
 		.PORT = &PORTD,
+		.STATUS = &PIND,
 		.pinNumber = 0b00000010
 	};
 	
 	struct_GPIO D2 = {
 		.DDR = &DDRD,
 		.PORT = &PORTD,
+		.STATUS = &PIND,
 		.pinNumber = 0b00000100
 	};
 		
 	struct_GPIO D3 = {
 		.DDR = &DDRD,
 		.PORT = &PORTD,
-		.PIN = &PIND,
+		.STATUS = &PIND,
 		.pinNumber = 0b00001000
 	};
 	
 	struct_GPIO D4 = {
 		.DDR = &DDRD,
 		.PORT = &PORTD,
+		.STATUS = &PIND,
 		.pinNumber = 0b00010000
 	};
 	
 	struct_GPIO D5 = {
 		.DDR = &DDRD,
 		.PORT = &PORTD,
+		.STATUS = &PIND,
 		.pinNumber = 0b00100000
 	};
 	
 	struct_GPIO D6 = {
 		.DDR = &DDRD,
 		.PORT = &PORTD,
+		.STATUS = &PIND,
 		.pinNumber = 0b01000000
 	};
 	
 	struct_GPIO D7 = {
 		.DDR = &DDRD,
 		.PORT = &PORTD,
+		.STATUS = &PIND,
 		.pinNumber = 0b10000000
 	};
 	/* конец определения всех возможных GPIO*/
@@ -187,24 +207,80 @@ struct_GPIO struct_GPIO_changeName(struct_GPIO from_A)	{
 	return bufer_GPIO;
 	};
 
-byte sensor_button_check(struct_GPIO GPIO_IN, struct_GPIO GPIO_OUT, int check_repeat, int check_limit)	{
+byte sensor_button_check_PINC(struct_GPIO GPIO_OUT, struct_GPIO GPIO_IN, int check_repeat, int check_limit, byte byte_output)	{
 	int check_count = 0;
-
+	byte result = 0;
+	
 	for (int i = 0; i<check_repeat; i++)  {
 		pinMode(GPIO_OUT, OUTPUT);
 		digitalWrite(GPIO_OUT, LOW);	// BUT_COM_OUT -> в низком уровне
 		pinMode(GPIO_IN, OUTPUT);	// BUT_1_IN -> сконфигурирован на выход
 		digitalWrite(GPIO_IN, LOW);	// BUT_1_IN -> в низком уровне
+		delay_counters(100);
 		pinMode(GPIO_IN, INPUT);		// BUT_1_IN -> сконфигурирован на вход
 		
 		digitalWrite(GPIO_OUT, HIGH);	// BUT_COM_OUT -> в высоков уровне
-		while ( (*GPIO_OUT.PIN & (GPIO_OUT.pinNumber) )==0 )  {
+		//while ( (*GPIO_IN.STATUS & (GPIO_IN.pinNumber) )==0 )  {
+		while ( ( PINC & (GPIO_IN.pinNumber) )==0 )  {	
+			check_count++;
+			}
+		}
+		
+	if (check_count > check_limit) {
+		result = byte_output;
+		} 
+		
+		return result;
+}
+
+byte sensor_button_check_PIND(struct_GPIO GPIO_OUT, struct_GPIO GPIO_IN, int check_repeat, int check_limit, byte byte_output)	{
+	int check_count = 0;
+	byte result = 0;
+	
+	for (int i = 0; i<check_repeat; i++)  {
+		pinMode(GPIO_OUT, OUTPUT);
+		digitalWrite(GPIO_OUT, LOW);	// BUT_COM_OUT -> в низком уровне
+		pinMode(GPIO_IN, OUTPUT);	// BUT_1_IN -> сконфигурирован на выход
+		digitalWrite(GPIO_IN, LOW);	// BUT_1_IN -> в низком уровне
+		delay_counters(100);
+		pinMode(GPIO_IN, INPUT);		// BUT_1_IN -> сконфигурирован на вход
+		
+		digitalWrite(GPIO_OUT, HIGH);	// BUT_COM_OUT -> в высоков уровне
+		//while ( (*GPIO_IN.STATUS & (GPIO_IN.pinNumber) )==0 )  {
+		while ( ( PIND & (GPIO_IN.pinNumber) )==0 )  {
 			check_count++;
 		}
 	}
+	
 	if (check_count > check_limit) {
-		return 1;
-		} else  {
-		return 0;
+		result = byte_output;
 	}
+	
+	return result;
+}
+
+byte sensor_button_check_PINB(struct_GPIO GPIO_OUT, struct_GPIO GPIO_IN, int check_repeat, int check_limit, byte byte_output)	{
+	int check_count = 0;
+	byte result = 0;
+	
+	for (int i = 0; i<check_repeat; i++)  {
+		pinMode(GPIO_OUT, OUTPUT);
+		digitalWrite(GPIO_OUT, LOW);	// BUT_COM_OUT -> в низком уровне
+		pinMode(GPIO_IN, OUTPUT);	// BUT_1_IN -> сконфигурирован на выход
+		digitalWrite(GPIO_IN, LOW);	// BUT_1_IN -> в низком уровне
+		delay_counters(100);
+		pinMode(GPIO_IN, INPUT);		// BUT_1_IN -> сконфигурирован на вход
+		
+		digitalWrite(GPIO_OUT, HIGH);	// BUT_COM_OUT -> в высоков уровне
+		//while ( (*GPIO_IN.STATUS & (GPIO_IN.pinNumber) )==0 )  {
+		while ( ( PINB & (GPIO_IN.pinNumber) )==0 )  {
+			check_count++;
+		}
+	}
+	
+	if (check_count > check_limit) {
+		result = byte_output;
+	}
+	
+	return result;
 }
