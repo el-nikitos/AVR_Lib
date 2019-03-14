8b6deaf44758ce46e233c0578bbb7f2473bb9480
AntonNikitin <kb.nikitinav@mail.ru>
2019-03-14 20:21:05 +0300

    автоматизирована функция выбора скорости

--- UART_Serial.h
+++ UART_Serial.h
@@ -8,20 +8,23 @@ typedef enum UART_speed	{
 } UART_speed;
 
 
-void UART_open(unsigned char ubrr)	{
+void UART_open(unsigned long Fcpu, unsigned long BaudRate)	{
+	double k = ((Fcpu/16) / BaudRate) - 1;
+	
+	unsigned char ubrr = round(k);
 	//UBRRH = (unsigned char)(ubrr>>8);
 	UBRRH = 0;
 	UBRRL = (unsigned char)(ubrr);
 	
 	
-	UCSRB = (1<<RXEN)|(1<<TXEN);
+	UCSRB = (1<<RXEN)|(1<<TXEN)|(1<<RXCIE);
 	UCSRC = (1<<URSEL)|(1<<USBS)|(3<<UCSZ0);
 }
  
  
  void UART_write(byte data)	{
 	 while ( !((UCSRA)&(1<<UDRE)) );
-	 
+	  
 	 UDR = data;
  }
  
