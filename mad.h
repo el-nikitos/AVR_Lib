77e9616184947933c3f2cf8aed10825ba1a096b0
Unknown <anton.v.n@yandex.ru>
2019-04-07 15:20:22 +0300

    файлы, необходимые для настройки arduino ide для работы с atmega8
    добавлены прошивка (вместо бутлодера), заголовочник девайнов и обновлен boards с рабочим содержимым

---
+++ mad.h
@@ -0,0 +1,22 @@
+#define PD0 0
+#define PD1 1
+#define PD2 2
+#define PD3 3
+#define PD4 4
+#define PD5 5
+#define PD6 6
+#define PD7 7
+
+#define PB0 8
+#define PB1 9
+#define PB2 10
+#define PB3 11
+#define PB4 12
+#define PB5 13
+
+#define PC0 14
+#define PC1 15
+#define PC2 16
+#define PC3 17
+#define PC4 18
+#define PC5 19
